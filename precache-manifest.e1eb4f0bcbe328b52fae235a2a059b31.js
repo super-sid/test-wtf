10c7194efc1737a7afdc58a8973edf3b5cbe8225
super-sid <siddhant.ag7@gmail.com>
2019-10-26 23:30:45 +0530

    Updates

---
+++ precache-manifest.e1eb4f0bcbe328b52fae235a2a059b31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1353d571fdabd03e9cd5a2728a0d3098",
+    "url": "/test-wtf/index.html"
+  },
+  {
+    "revision": "c69279d6747a092cef71",
+    "url": "/test-wtf/static/css/main.3604449f.chunk.css"
+  },
+  {
+    "revision": "1a3bc157ad541d738a66",
+    "url": "/test-wtf/static/js/2.f136b48a.chunk.js"
+  },
+  {
+    "revision": "c69279d6747a092cef71",
+    "url": "/test-wtf/static/js/main.682004a4.chunk.js"
+  },
+  {
+    "revision": "657dcc30b1cd336b8757",
+    "url": "/test-wtf/static/js/runtime-main.8da32f3c.js"
+  }
+]);
